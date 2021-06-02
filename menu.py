2bad490da060169477eb92685f6b7769a6f85aa8
EthanWill059 <ewilliams8222@gmail.com>
2021-06-03 10:26:08 +1200

    dobule periods work

---
+++ menu.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk() # defines the window itself, root is the parent window
+root.size(400, 400)
+
+
+
+
+
+root.mainloop() ## Runs a loop for the window
