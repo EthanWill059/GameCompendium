6acec0a016b82f4d3939e70682cb6b83b90388a0
EthanWill059 <ewilliams8222@gmail.com>
2021-06-05 22:20:45 +1200

    V0.1.0

--- menu.py
+++ menu.py
@@ -1,11 +1,35 @@
+'''
+The menu file for the Game compendium project
+'''
+
+#### IMPORTS ####
 import tkinter as tk
+from tkinter import *
 from tkinter import ttk
+#################
 
-root = tk.Tk() # defines the window itself, root is the parent window
-root.size(400, 400)
+#### Functions ####
+def rungame():
+    print("GAME BEING RUN")
 
+#### MAIN PROGRAM ####
 
+root = tk.Tk(); print("Window Started") # defines the window itself, root is the parent window and prints a notification to notate this event
+root.title("Game Compendium") # sets the title shown in the top bar
 
+title = Label(root, text="Game Compendium") # Creates the actual title in the menu
+title.grid(row=0, column=0, padx=20, pady=30, sticky=NSEW) # places the title in the grid location (0,0)
 
+games_frame = LabelFrame(root, text="Games") # creates an onscreen box to clean up the GUI contains the run buttons for games
+games_frame.grid(row=1, column=0, padx=50, pady=30) # places this on screen at grid location (1,0)
 
-root.mainloop() ## Runs a loop for the window
+cargame_button = Button(games_frame, text="Car Racer Game", command=rungame) # first game button, starts car racer
+cargame_button.grid(row=0, column=0, padx=10, pady=20, sticky=NSEW)
+
+highlow_button = Button(games_frame, text="Higher or Lower Game", command=rungame) # first game button, starts car racer
+highlow_button.grid(row=1, column=0, padx=10, pady=20, sticky=NSEW)
+
+quit_button = Button(root, text="QUIT", command=quit)
+quit_button.grid(row=3, column=0, padx=10, pady=20, sticky=EW)
+
+root.mainloop()## Runs a loop for the window 
