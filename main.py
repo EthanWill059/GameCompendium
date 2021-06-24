28642b67b89072f668deed94bfbc0638d9517c35
EthanWill059 <81672961+EthanWill059@users.noreply.github.com>
2021-06-24 21:33:56 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 '''
 The menu file for the Game compendium project
 '''
-
-#### IMPORTS ####
-##
-import games.carracer as cargame# imports the car racer game
-import games.highorlow as hlgame# imports the higher or lower game
-#################
-
 #### Functions ####
-
-
-#### Varibles ####
-flag = False # used for exiting a while loop without break
-welcomeplayed = False
-
+def highorlow():
+    print("game ran")
+    
+def carracer():
+    print("game ran")
+    
 #### MAIN PROGRAM ####
 
-while flag == False: # loop until flag is raised
-    try: # error catching try block
-        if welcomeplayed == False: # stops the welcome message from playing over and over again
-            print("""
+while True: # loop until flag is raised
+    print("""
     Welcome to the Game Compendium
 
     Please type a number to choose a game
@@ -29,22 +20,18 @@ while flag == False: # loop until flag is raised
 
     2) Higher or Lower Game
 
-        """)
-        welcomeplayed = True
+    """)
+        
 
-        user_input = int(input("Make a Selection << -1 to quit >> ")) # input a number, int
+    user_input = int(input("Make a Selection << -1 to quit >> ")) # input a number, int
 
-        if user_input == 1: # if the input is equal to 1
-            welcomeplayed = False # play the welcome message when user returns
-            cargame.run()
-        elif user_input == 2: # if the input is equal to 2
-            welcomeplayed = False # plays welcome message on user return
-            hlgame.run() # runs the higher or lower game
-        elif user_input == -1: # if the input is equal to -1
-            flag = True # raises flag
-        else: # any thing else
-            print("Invalid Input, Please enter a Number in range")
-    except ValueError: # catching invalid inputs
-        print("Invalid Input, Please Enter a Number")
+    if user_input == 1: # if the input is equal to 1
+        carracer()
+            
+    elif user_input == 2: # if the input is equal to 2
+        highorlow()
+
+    elif user_input == -1: # if the input is equal to -1
+        break
 
 print("Thank You and Goodbye!")
